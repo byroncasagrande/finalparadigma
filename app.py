e33a2019b11e87623c7cb7c587e854d4c97efd67
byroncasagrande <byron.collazo@casagrande.edu.ec>
2025-01-04 01:56:39 +0000

    EliminarArchivoErroneo

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import cargaarchivo as ca
 import generatestatistics as ge
-import generatecsv as gc
 from generateregression import realizar_regresion
 from generatereport import generar_reporte
 
